a7e02bc63f47deefb9f9204c4b89faf9b033c6d5
Chase Florell <chase.florell@gmail.com>
2020-04-21 14:47:43 -0700

    Initial Commit

---
+++ ColorConverterCrash/MainPageModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ColorConverterCrash
+{
+    public class MainPageModel
+    {
+        public MainPageModel()
+        {
+        }
+
+        public string MyColor { get; set; } = "";
+    }
+}
